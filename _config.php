8ce994d6e61c9770df0ad65e03d8cab80d4c47d4
seppzzz <till.moser@ufg.ac.at>
2022-11-11 10:29:58 +0100

    Add files via upload

---
+++ _config.php
@@ -0,0 +1,15 @@
+<?php
+
+use SilverStripe\Core\Manifest\ModuleLoader;
+use SilverStripe\Forms\HTMLEditor\TinyMCEConfig;
+
+
+call_user_func(function () {
+    $module = ModuleLoader::inst()->getManifest()->getModule('seppzzz/silverstripe-phonelink');
+	
+    // Enable insert-link to phone numbers
+    TinyMCEConfig::get('cms')
+        ->enablePlugins([
+            'sslinkphone' => $module->getResource('client/dist/js/TinyMCE_sslink-phone.js'),
+        ]);
+});
