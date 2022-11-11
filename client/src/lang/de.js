8a4fb0bd912b4077cb2513e6a786e3c1de64d15c
seppzzz <till.moser@ufg.ac.at>
2022-11-11 09:04:52 +0100

    Delete client directory

--- client/src/lang/de.js
+++
@@ -1,4 +0,0 @@
-ss.i18n.addDictionary('de', {
-	"Admin.LINKLABEL_PHONE": "Telefonnummer verlinken",
-	"Admin.LINK_PHONE": "Telefonnummer verlinken",
-});
--- client/src/lang/fr.js
+++
@@ -1,8 +0,0 @@
-if(typeof(i18n) == 'undefined') {
-    console.error('Class i18n not defined');
-} else {
-    i18n.addDictionary('fr', {
-        "Admin.LINKLABEL_PHONE": "Lien vers numéro de téléphone",
-        "Admin.LINK_PHONE": "Inserer un lien vers un numéro de téléphone",
-    });
-}
